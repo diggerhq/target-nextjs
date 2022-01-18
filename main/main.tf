41a0650706253f1de55e01696cb46d5ee43f6630
alexey <alexey@digger.dev>
2022-01-18 16:22:26 +0000

    add nextjs files for test

--- main/main.tf
+++ main/main.tf
@@ -16,13 +16,25 @@ terraform {
       version = "= 1.4.0"
       source  = "hashicorp/local"
     }
-
-    template = {
-      version = "= 2.1.2"
-      source  = "hashicorp/template"
-    }
   }
 }
+
+# Provider used for creating the Lambda@Edge function which must be deployed
+# to us-east-1 region (Should not be changed)
+provider "aws" {
+  alias  = "global_region"
+  region = "us-east-1"
+}
+
+module "tf_next" {
+  source = "github.com/diggerhq/terraform-aws-next-js"
+
+  providers = {
+    aws.global_region = aws.global_region
+  }
+  next_tf_dir = "./nextjs_app"
+}
+
 
 # The AWS Profile to use
 # variable "aws_profile" {
