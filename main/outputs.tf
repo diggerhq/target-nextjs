1b2255057cfdeb6adec3f01dbc5f5e6ff6b0466e
alexey <alexey@digger.dev>
2022-01-26 17:36:01 +0000

    fix deployment_name

--- main/outputs.tf
+++ main/outputs.tf
@@ -1,3 +1,3 @@
 output "nextjs_deployment_name" {
-  value = "${var.environment}-${random_string.unique_deployment_id}"
+  value = "${var.environment}-${random_string.unique_deployment_id.result}"
 }
