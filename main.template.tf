cf3e70c758c674f4d4a1f0d5a58338c5aabc00dc
alexey <alexey@digger.dev>
2022-05-04 14:36:31 +0100

    add dggr credentials

--- main.template.tf
+++ main.template.tf
@@ -5,8 +5,8 @@ terraform {
   # vars are not allowed in this block
   # see: https://github.com/hashicorp/terraform/issues/22088
   backend "s3" {
-  access_key = var.dggr_aws_key
-  secret_key = var.dggr_aws_secret
+    access_key = "{{dggr_aws_key}}"
+    secret_key = "{{dggr_aws_secret}}"
   }
 
   required_providers {
@@ -16,11 +16,11 @@ terraform {
     }
 
     archive = {
-      source  = "hashicorp/archive"
+      source = "hashicorp/archive"
     }
 
     local = {
-      source  = "hashicorp/local"
+      source = "hashicorp/local"
     }
   }
 }
@@ -28,14 +28,14 @@ terraform {
 # Provider used for creating the Lambda@Edge function which must be deployed
 # to us-east-1 region (Should not be changed)
 provider "aws" {
-  region  = var.region
+  region     = var.region
   access_key = var.aws_key
   secret_key = var.aws_secret
 }
 
 provider "aws" {
-  alias  = "global_region"
-  region = "us-east-1"
+  alias      = "global_region"
+  region     = "us-east-1"
   access_key = var.aws_key
   secret_key = var.aws_secret
 }
--- terraform.template.tfvars
+++ terraform.template.tfvars
@@ -1,8 +1,6 @@
 
 aws_key           = "{{aws_key}}"
 aws_secret        = "{{aws_secret}}"
-aws_key           = "{{dggr_aws_key}}"
-aws_secret        = "{{dggr_aws_secret}}"
 project_name      = "{{app_name}}"
 environment       = "{{environment}}"
 region            = "{{region}}"
